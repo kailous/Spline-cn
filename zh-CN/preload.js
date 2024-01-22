8ef83e301ad9bb47261d049319380595b2600ea9
kailous <kailous@live.cn>
2024-01-22 22:53:06 +0800

    first commit

---
+++ zh-CN/preload.js
@@ -0,0 +1,27 @@
+const { ipcRenderer } = require('electron');
+
+// 汉化逻辑封装在一个函数中
+function applyLocalization() {
+    var script = document.createElement("script");
+    var header = document.getElementsByTagName("head")[0];
+    header.appendChild(script);
+    script.src = 'https://zdo.fun/splineCN.js';
+    var option = {
+        title: '欢迎使用Spline汉化版，首次使用请先登录',
+        body: '晗萧汉化组 zdo.fun'
+    };
+    var myNotification = new window.Notification(option.title, option);
+}
+
+
+
+// 监听来自主进程的语言切换事件
+ipcRenderer.on('change-language', (event, language) => {
+    if (language === 'english') {
+        // 输出语言切换事件
+        console.log('change-language', language);
+    } else {
+        // 输出语言切换事件
+        console.log('change-language', language);
+    }
+});
