a57c8231ea146eb1fd241e6d4feaeabf8203a62a
kailous <kailous@live.cn>
2024-01-23 10:11:24 +0800

    放弃在应用内切换语言，将代码简化到最简单。

--- zh-CN/preload.js
+++ zh-CN/preload.js
@@ -1,24 +1,4 @@
-const { ipcRenderer } = require('electron');
-
-// 监听来自主进程的语言切换事件
-ipcRenderer.on('change-language', (event, language) => {
-    if (language === 'english') {
-        // 实现英文界面的逻辑
-        applyEnglishLocalization();
-    } else if (language === 'chinese') {
-        // 实现中文界面的逻辑
-        applyChineseLocalization();
-    }
-});
-
-function applyEnglishLocalization() {
-    // 在这里添加英文本地化的代码
-    console.log('切换到英文界面');
-}
-
-function applyChineseLocalization() {
-    // 在这里添加中文本地化的代码
-    console.log('切换到中文界面');
+setTimeout(function(){
     var script = document.createElement("script");
     var header = document.getElementsByTagName("head")[0];
     header.appendChild(script);
@@ -27,5 +7,5 @@ function applyChineseLocalization() {
         title: '欢迎使用Spline汉化版，首次使用请先登录',
         body: '晗萧汉化组 zdo.fun'
     };
-    var myNotification = new window.Notification(option.title, option);
-}
+    var myNotification = new window.Notification(option.title,option);
+},600)
